3e69e2ac6fc27445c6f1acfe4dcd0be2b14a2a10
Niftiyev Kenan <sd7zaw7az@code.edu.az>
2024-06-22 23:22:06 +0400

    footerscss

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,8 +3,10 @@ import style from './Footer.module.scss'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className={style.footer}>
+      <div className={style.container}>
+
+      </div>
     </div>
   )
 }
